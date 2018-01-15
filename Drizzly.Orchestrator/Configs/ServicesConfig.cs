4dbc72474ddea7e3d67a7c11d35125ada6a419cf
Valentin <contact@vfrz.fr>
2018-01-15 15:33:25 +0100

    First commit

---
+++ Drizzly.Orchestrator/Configs/ServicesConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Drizzly.Orchestrator.Configs
+{
+    public class ServicesConfig
+    {
+        public Dictionary<string, string> EndPoints { get; set; }
+    }
+}
---
+++ Drizzly.Orchestrator/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Drizzly.Commons;
+using Drizzly.Orchestrator.Configs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RestSharp;
+using ResponseStatus = Drizzly.Commons.ResponseStatus;
+
+namespace Drizzly.Orchestrator.Controllers
+{
+    [Route("api")]
+    public class ValuesController : Controller
+    {
+        private readonly ServicesConfig _configuration;
+
+        public ValuesController(IOptions<ServicesConfig> configuration)
+        {
+            _configuration = configuration.Value;
+        }
+
+        [HttpGet("{service}/{value}")]
+        public async Task<IActionResult> Get(string service, string value)
+        {
+            service = service.ToLower();
+            value = value.ToLower();
+            if (!_configuration.EndPoints.ContainsKey(service))
+            {
+                return JsonResultExtensions.CreateEmptyResult(ResponseStatus.Error,
+                    $"No endpoint found for service {service}");
+            }
+
+            var client = new RestClient(_configuration.EndPoints[service]);
+            
+            var request = new RestRequest(value, Method.POST);
+
+            if (Request.HasFormContentType)
+            {
+                foreach (var key in Request.Form.Keys)
+                {
+                    request.AddParameter(key, Request.Form[key]);
+                }
+            }
+
+            var response = await client.ExecuteTaskAsync(request);
+
+            if (!response.IsSuccessful)
+            {
+                return JsonResultExtensions.CreateEmptyResult(ResponseStatus.Error,
+                    $"Query wasn't succesful, please try again later of contact Drizzly team. " +
+                    $"Service: {service}, Value: {value}");
+            }
+
+            return Json(JsonConvert.DeserializeObject(response.Content)).ToUnifiedResult();
+        }
+    }
+}
